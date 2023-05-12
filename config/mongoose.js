cc923b8758ae6debe64093f243124c129b4cb39b
Rahul Wadate <115419561+rahulwadate21@users.noreply.github.com>
2023-05-12 08:57:30 +0530

    setup mongoose

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 // connecting mongoose to its default server and ecommerceDB
-// mongoose.connect('mongodb+srv://soumyasri2245:Soumya22%4034@cluster0.u2ywt3o.mongodb.net/?retryWrites=true&w=majority');
+
 
 mongoose.connect('mongodb://127.0.0.1:27017/ecommerce-api', {
     useNewUrlParser: true
@@ -16,3 +16,58 @@ db.once('open', function(){
 });
 
 module.exports = db;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// mongoose.connect('mongodb+srv://soumyasri2245:Soumya22%4034@cluster0.u2ywt3o.mongodb.net/?retryWrites=true&w=majority');
