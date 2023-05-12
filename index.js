d2f9a1c61fdcc7bb36da69d7bfc66466636c162d
rahulwadate21 <115419561+rahulwadate21@users.noreply.github.com>
2023-05-12 08:48:20 +0530

    fire up server

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ app.use(bodyParser.urlencoded({extended: true}));
 // using routes
 app.use('/products', require('./routes/products'));
 
-// starting the server
+// starting the server..........
 app.listen(3000, function(){
     console.log('API is live on http://localhost:3000/products');
 });
